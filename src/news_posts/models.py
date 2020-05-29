c1b2533f74c25f901662d55033ae4935f644d9f8
ZenCrazyCat <zencrazycat@gmail.com>
2020-05-29 09:36:05 +0300

    add models Post and Comment, update .gitignore

--- src/news_posts/models.py
+++ src/news_posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    link = models.URLField(max_length=256)
+    creation_date = models.DateField(default=timezone.now)
+    upvotes = models.IntegerField(default=0)
+    author = models.CharField(max_length=128)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='posts')
+    author = models.CharField(max_length=128)
+    content = models.TextField()
+    creation_date = models.DateField(default=timezone.now)
