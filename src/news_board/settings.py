5d90d2ff5ceb979d3ed3c0d3c73081d164135c05
ZenCrazyCat <zencrazycat@gmail.com>
2020-05-29 19:38:38 +0300

    add static-settings

--- src/news_board/settings.py
+++ src/news_board/settings.py
@@ -102,6 +102,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, '..', "static_content", 'static')
 
 CELERY_BROKER_URL = 'amqp://{}:{}@{}:{}'.format(
     os.environ['RABBITMQ_DEFAULT_USER'],
